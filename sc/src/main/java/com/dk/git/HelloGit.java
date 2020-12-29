24a8867d7b02931c1aed9d29259bac9f3048e054
dk <113>
2020-12-29 20:25:12 +0800

    添加文件

---
+++ sc/src/main/java/com/dk/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.dk.git;
+
+/**
+ * @author : dk
+ * @date : 2020/12/29 20:53
+ * @description :
+ */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
